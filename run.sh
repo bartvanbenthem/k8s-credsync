d65f3d738fe21599202cc02c7618fb2543dc4025
Bart van benthem <bartvanbenthem@hotmail.com>
2021-01-04 14:40:59 +0100

    update run script

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# k8s.io/api kubernetes-1.19.0
-# k8s.io/apimachinery kubernetes-1.19.0
-# k8s.io/client-go kubernetes-1.19.0
+# go get k8s.io/api kubernetes-1.19.0
+# go get k8s.io/apimachinery kubernetes-1.19.0
+# go get k8s.io/client-go kubernetes-1.19.0
 
 export K8S_KUBECONFIG='/var/snap/microk8s/current/credentials/client.config'
 export K8S_PROXY_SECRET_NAME='loki-multi-tenant-proxy-auth-config'
